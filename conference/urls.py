9efd79701bcbeeb64aedb7c57e1c9c599894ac95
Uwibambe Sandrine <uwibambesandrine@gmail.com>
2023-06-19 17:43:40 +0100

    create conference id

--- conference/urls.py
+++ conference/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('conferences/', views.conferences_view),
     path('conferences/create', views.create_conference_view),
     path('conferences/<id>' , views.conference_id ),
+    
 ]
