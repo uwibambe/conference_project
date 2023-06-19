8d348c891272c1b3e9a2e46efe00fbefd1bf9279
Uwibambe Sandrine <uwibambesandrine@gmail.com>
2023-06-19 16:20:01 +0100

    get all conference

---
+++ conference/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+conferences = [
+        {
+            "id": 'A',
+            "name": "INFORMATION TECHNOLOGY",
+            "date": "2005-01-01",
+            "location": "HUYE"
+        },
+        {
+            "id": 'B',
+            "name": "COMPUTER SCIENCE",
+            "date": "2019-06-07",
+            "location": "NYAGATARE"
+        },
+        {
+            "id": 'C',
+            "name": "COMPUTER ENGINNER",
+            "date": "2022-04-12",
+            "location": "BUTARE"
+        },
+
+        {
+            "id": 'D',
+            "name": "ACCOUNTING",
+            "date": "2021-11-23",
+            "location": "ESFB"
+        },
+
+        {
+            "id": 'E', 
+            "name": "Forest",
+            "date": "2021-07-23",
+            "location": "kigali"
+        },
+    ]
+def conferences_view(request):
+    return render(request, 'conferences.html', context={"conferences": conferences})
